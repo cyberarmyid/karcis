f24d800d4d5862a6328c0eaaa4dd5f0e7596d688
Anhar Solehudin <anhsbolic@gmail.com>
2019-09-16 13:56:32 +0700

    set dot env

--- conn.php
+++ conn.php
@@ -1,10 +1,23 @@
 <?php
-$conn = mysqli_connect("localhost","guest","hulahulalo","db_karcis");
+
+require_once("./vendor/autoload.php");
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::create(__DIR__);
+$dotenv->load();
+
+$db_host = getenv("DB_HOST");
+$db_name = getenv("DB_DATABASE");
+$db_username = getenv("DB_USERNAME");
+$db_password = getenv("DB_PASSWORD");
+
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} 
+}
 
 $host = '/karcis/';
 
