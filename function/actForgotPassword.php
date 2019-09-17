3d3ab637891348ad87de47dd325613ecfa375f32
rendybustari <rendybustari@gmail.com>
2019-09-17 13:32:25 +0700

    forgot password

--- function/actForgotPassword.php
+++ function/actForgotPassword.php
@@ -3,13 +3,13 @@
 
     $email = $_POST['email'];
     $hash = sha1($email);
-    $link = $host."resetPassword.php?hash=".$hash;
+    $link = $_SERVER['HTTP_HOST'].$host."resetPassword.php?hash=".$hash;
 
     $sql = "INSERT INTO forgot_password (email, hash, link)
     VALUES ('$email', '$hash', '$link')";
 
     if ($conn->query($sql) === TRUE) {
-        header('location:confirmation.php?hash='.$hash);
+        header('location:'.$host.'confirmation.php?hash='.$hash);
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
